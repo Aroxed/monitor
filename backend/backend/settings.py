e2143a45547bab0a0c09407bd4d3c8e640ad2604
Aroxed <petrashenko@gmail.com>
2024-09-19 16:05:28 +0300

    email settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,22 +73,22 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'monitor',
-        'CLIENT': {
-            'host': 'localhost',
-        }
-    }
+     "default": {
+         "ENGINE": "django.db.backends.sqlite3",
+         "NAME": BASE_DIR / "db.sqlite3",
+     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'djongo',
+#        'NAME': 'monitor',
+#        'CLIENT': {
+#            'host': 'localhost',
+#        }
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
